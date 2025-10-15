cd05d1b3341360857260c7aa8cddd0f09c1834df
Dexx van der Mandele <dvandermandele@chathamfinancial.com>
2025-10-15 13:10:35 -0600

    Add basic card viewer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
+<script lang="ts">
+		interface Spell {
+			index: string;
+			name: string;
+			level: number;
+			url: string;
+		};
+  let cards: Spell[] = [
+    {"index":"acid-arrow","name":"Acid Arrow","level":2,"url":"/api/2014/spells/acid-arrow"},
+  ];
+</script>
+
 <h1>Spards</h1>
+{#each cards as card}
+  <div>
+    <h2>{card.name}</h2>
+    <p>Level: {card.level}</p>
+  </div>
+{/each}
