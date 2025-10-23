4e8fffd17dda5e09f60585a2fc8526fa8f86b413
Dexx van der Mandele <dvandermandele@chathamfinancial.com>
2025-10-23 16:03:22 -0600

    Restore print-all-SRE-cards functionality

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -8,49 +8,51 @@ export interface Spell {
 	url: string;
 }
 
-// interface GetSpellsResponse {
-// 	count: number;
-// 	results: Spell[];
-// }
+interface GetSpellsResponse {
+	count: number;
+	results: Spell[];
+}
 
-// export const load: PageServerLoad = async ({ fetch }) => {
-// 	const response = await fetch('https://www.dnd5eapi.co/api/spells');
-// 	const data: GetSpellsResponse = await response.json();
+export const load: PageServerLoad = async ({ fetch }) => {
+	const response = await fetch('https://www.dnd5eapi.co/api/spells');
+	const data: GetSpellsResponse = await response.json();
+	const spellRequests = data.results.map(spell => fetchSpellDetails(spell.index));
+  const spells = await Promise.all(spellRequests) as SpellDetails[];
 
-// 	return {
-// 		spells: data.results
-// 	};
-// };
-
-export const load: PageServerLoad = async () => {
-	const galSpellRequests: Promise<SpellDetails | string>[] = [
-		'toll-the-dead',
-		'silvery-barbs',
-		'protection-from-evil-and-good',
-		'levitate',
-		'sending',
-		'message',
-		'alarm',
-		'identify',
-		'flaming-sphere',
-		'chill-touch',
-		'burning-hands',
-		'mage-armor',
-		'magic-missile',
-		'shield',
-		'counterspell',
-		'invisibility',
-		'mage-hand',
-		'hold-person',
-		'misty-step',
-		'scorching-ray',
-		'mending',
-		'comprehend-languages',
-		'charm-person',
-		'detect-magic',
-		// too big
-		// { index: 'animate-dead' },
-	].map(index => fetchSpellDetails(index));
-  const spells = await Promise.all(galSpellRequests);
 	return { spellInfo: spells.map(s => ({ spell: s, error: typeof s === 'string' ? s : undefined })) };
+
 };
+
+// export const load: PageServerLoad = async () => {
+// 	const galSpellRequests: Promise<SpellDetails | string>[] = [
+// 		'toll-the-dead',
+// 		'silvery-barbs',
+// 		'protection-from-evil-and-good',
+// 		'levitate',
+// 		'sending',
+// 		'message',
+// 		'alarm',
+// 		'identify',
+// 		'chill-touch',
+// 		'mage-armor',
+// 		'shield',
+// 		'counterspell',
+// 		'mage-hand',
+// 		'misty-step',
+// 		'mending',
+// 		'comprehend-languages',
+// 		'detect-magic',
+// 		'flaming-sphere',
+// 		'burning-hands',
+// 		'magic-missile',
+// 		'invisibility',
+// 		'hold-person',
+// 		'scorching-ray',
+// 		'charm-person',
+// 		'phantom-steed',
+// 		'animate-dead',
+// 		'find-familiar',
+// 	].map(index => fetchSpellDetails(index));
+//   const spells = await Promise.all(galSpellRequests);
+// 	return { spellInfo: spells.map(s => ({ spell: s, error: typeof s === 'string' ? s : undefined })) };
+// };
