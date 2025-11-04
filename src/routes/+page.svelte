8199f6e000d06e668b048f51eec5372dbfd6c6ce
Dexx van der Mandele <dvandermandele@chathamfinancial.com>
2025-11-04 13:29:11 -0700

    Eliminate gap between sets of cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,17 +10,25 @@
   // let truncatedCards = $derived(data.spells.slice(0, 10));
 </script>
 
-{#each data.spells as spell}
-  {@const size = getSizeClass(spell)}
-  <Card title={spell.name} size={size}>
-    <SpellCardContent spell={spell} size={size} />
-  </Card>
-{/each}
-{#each data.monsters as monster}
-  <Card title={monster.name} size='mid'>
-    <MonsterCardContent monster={monster} />
-  </Card>
-{/each}
-{#each data.errors as error}
-  <Card title='Error' size='mid'><span>{error}</span></Card>
-{/each}
+<div class="page-container">
+  {#each data.spells as spell}
+    {@const size = getSizeClass(spell)}
+    <Card title={spell.name} size={size}>
+      <SpellCardContent spell={spell} size={size} />
+    </Card>
+  {/each}
+  {#each data.monsters as monster}
+    <Card title={monster.name} size='mid'>
+      <MonsterCardContent monster={monster} />
+    </Card>
+  {/each}
+  {#each data.errors as error}
+    <Card title='Error' size='mid'><span>{error}</span></Card>
+  {/each}
+</div>
+
+<style>
+  .page-container {
+    font-size: 0;
+  }
+</style>
